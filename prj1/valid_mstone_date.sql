f00dfee38c970ed23db7b374f6f1613ef63a0492
Mingfei Ge <mg3534@columbia.edu>
2014-10-27 02:33:08 -0400

    Change verification method s.t. deadline between plan's start_date and end_date

--- prj1/valid_mstone_date.sql
+++ prj1/valid_mstone_date.sql
@@ -1,9 +1,20 @@
 create or replace trigger valid_mstone_date
-before insert on mstone
+before insert or update on mstone
 for each row
+when (new.deadline <> old.deadline)
+
+declare
+	v_stdate plan.start_date%type;
+	v_eddate plan.end_date%type;
 
 begin
-	if :new.deadline < sysdate
+	Select start_date, end_date
+	  into v_stdate,v_eddate
+	  from plan
+	 where plan.plan_id = :new.plan_id;
+	--dbms_output.put_line(v_stdate);
+	--dbms_output.put_line(v_eddate);
+	if :new.deadline> v_eddate or :new.deadline < v_stdate
 	then
 	RAISE_APPLICATION_ERROR(-20105,'deadline denied');
 	end if;
