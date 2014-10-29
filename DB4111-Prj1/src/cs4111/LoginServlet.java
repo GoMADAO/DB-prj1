cf0cb92ca30d325b0c1a94f47f81e64527856bc2
AlexandraVon <305120283@qq.com>
2014-10-29 12:54:47 -0400

    loginServlet, success, error

--- DB4111-Prj1/src/cs4111/LoginServlet.java
+++ DB4111-Prj1/src/cs4111/LoginServlet.java
@@ -1,6 +1,8 @@
 package cs4111;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -34,6 +36,30 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		//设置HTTP响应的文档类型,此处为Text/html,如果更改为application\msword则设置为word文档格式  
+        response.setContentType("text/html");  
+        //设置响应所采用的编码方式  
+        response.setCharacterEncoding("GB18030");  
+        //取得参数username的值  
+        String stuname=request.getParameter("username");  
+        String passwd=request.getParameter("password");  
+          
+          
+        Stubean stu=new Stubean();
+        stu.setName(stuname);  
+        stu.setPwd(passwd);  
+        CheckStu ckstu=new CheckStu();  
+        boolean bool=ckstu.checkStu(stu);  
+          
+        String forward;  
+        if(bool){  
+            forward="success.jsp";  
+              
+        }else{  
+            forward="error.jsp";  
+        }  
+        RequestDispatcher rd=request.getRequestDispatcher(forward);
+        rd.forward(request,response);  
 	}
 
 }
